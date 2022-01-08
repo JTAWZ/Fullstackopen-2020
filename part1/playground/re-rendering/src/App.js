4535fe6d2d06da09b89203e9cf60bac38af01c8f
Jason <Tiongrealty@gmail.com>
2022-01-09 00:18:41 +0800

    Event Handling

--- part1/playground/re-rendering/src/App.js
+++ part1/playground/re-rendering/src/App.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
+const Display = ({ counter }) => <div>{counter}</div>;
 
-  setTimeout(() => {
-    setCounter(counter + 1);
-  }, 1000);
-
-  return <div>{counter}</div>;
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
-// const App = (props) => {
-//   const { counter } = props;
-//   return (
-//     <div>
-//       <p> Hello </p>
-//       {counter}
-//     </div>
-//   );
-// };
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const increaseByone = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+
+  // setTimeout(() => {
+  //   setCounter(counter + 1);
+  // }, 1000);
+
+  return (
+    <>
+      <Display counter={counter} />
+      <Button onClick={increaseByone} text="Plus one" />
+      <Button onClick={decreaseByOne} text="Minus one" />
+      <Button onClick={setToZero} text="Reset" />
+    </>
+  );
+};
 
 export default App;
