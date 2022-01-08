c3be432cf19dda3d5485f5e82d50b759cb2b83d2
Jason <Tiongrealty@gmail.com>
2022-01-08 21:21:53 +0800

    Stateful Component

--- part1/playground/re-rendering/src/App.js
+++ part1/playground/re-rendering/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = (props) => {
-  const { counter } = props;
-  return (
-    <div>
-      <p> Hello </p>
-      {counter}
-    </div>
-  );
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
+
+  return <div>{counter}</div>;
 };
+
+// const App = (props) => {
+//   const { counter } = props;
+//   return (
+//     <div>
+//       <p> Hello </p>
+//       {counter}
+//     </div>
+//   );
+// };
 
 export default App;
