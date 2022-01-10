eebc5f1d81a8d19d250a1eeb0de72f0f705beb02
Jason <Tiongrealty@gmail.com>
2022-01-10 22:27:34 +0800

    Handling Arrays - concat, push , spread methods

--- part1/playground/complex-state/src/App.js
+++ part1/playground/complex-state/src/App.js
@@ -1,35 +1,55 @@
 import { React, useState } from "react";
 
 const App = () => {
-  const [click, setClick] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
 
-  const handleRightClick = () => {
-    setClick({ ...click, right: click.right + 1 });
+  // Different methods to add value in an array during setState
+  // for .push() and spread operator, you need to define the type during setState
+
+  // const handleRightClick1 = () => {
+  //   setAllClicks([allClicks.push("R")]);
+  //   setRight(right + 1);
+  // };
+
+  // const handleRightClick2 = () => {
+  //   setAllClicks([...allClicks, "R"]);
+  //   setRight(right + 1);
+  // };
+
+  const handleRightClick3 = () => {
+    setAllClicks(allClicks.concat("R"));
+    setRight(right + 1);
   };
 
-  const handleleftClick = () => {
-    setClick({ ...click, left: click.left + 1 });
+  // const handleleftClick1 = () => {
+  //   setAllClicks([allClicks.push("L")]);
+  //   setLeft(left + 1);
+  // };
+
+  // const handleleftClick2 = () => {
+  //   setAllClicks([...allClicks, "L"]);
+  //   setLeft(left + 1);
+  // };
+
+  const handleleftClick3 = () => {
+    setAllClicks(allClicks.concat("L"));
+    setLeft(left + 1);
   };
 
-  const handleSpecialClick = () => {
-    setClick({ ...click, left: click.left++ });
-  };
   return (
     <div>
       <div>
-        {click.left}
-        <button onClick={handleleftClick}>left</button>
+        {left}
+        <button onClick={handleleftClick3}>left</button>
       </div>
       <div>
-        {click.right}
-        <button onClick={handleRightClick}>right</button>
+        {right}
+        <button onClick={handleRightClick3}>right</button>
       </div>
-      <div>
-        <button onClick={handleSpecialClick}>special</button>
-      </div>
+      <p>{allClicks.join("")}</p>
+      <p>{allClicks.length}</p>
     </div>
   );
 };
