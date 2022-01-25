3b0e8b3081def0e6d9a46be46d3fc63216097972
Jason <Tiongrealty@gmail.com>
2022-01-26 00:40:55 +0800

    part2a:refactoring modules

---
+++ part2/playground/notes/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Notes from "./Note";
+
+const App = ({ notes }) => {
+  // const listOfNote = notes.map((note) => <li key={note.id}>{note.content}</li>);
+  // const listofNotes = notes.map(function (note) {
+  //   return <li key={note.id}>{note.content}</li>;
+  // });
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Notes key={note.id} notes={note} />
+          // <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/playground/notes/src/Note.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Notes = ({ notes }) => {
+  return <li>{notes.content}</li>;
+};
+
+export default Notes;
