010731133e0a0c56cddc30512a405f9e09d829f1
Jason <Tiongrealty@gmail.com>
2022-10-05 23:27:58 +0800

    part2:exercise2.14-completed

--- part2/countries/src/Component/Countries.js
+++ part2/countries/src/Component/Countries.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Countries = ({ searchResult }) => {
   console.log("check search results", searchResult);
@@ -53,6 +54,7 @@ const CountryInfomation = ({ result }) => {
       <h2>Languagues:</h2>
       <Languages languages={result.languages} />
       <img src={result.flags.png} alt="country flag" />
+      <WeatherInformation result={result} />
     </React.Fragment>
   );
 };
@@ -67,6 +69,45 @@ const Languages = ({ languages }) => {
     );
   });
   return displayLanguage;
+};
+const Img = ({ img }) => {
+  return <img src={img} alt="flag" width="100px" />;
+};
+const WeatherInformation = ({ result }) => {
+  const api_key = process.env.REACT_APP_API_KEY;
+  const lat = result.capitalInfo.latlng[0];
+  const lng = result.capitalInfo.latlng[1];
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}&units=metric`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, []);
+
+  console.log("display lat long result", lat, lng);
+
+  const weathericon = JSON.stringify(weather.weather.icon);
+  return (
+    <>
+      {Object.keys(weather).length === 0 || weather.error ? (
+        <h2>'Error'</h2>
+      ) : (
+        <>
+          <h2>Weather in {result.capital}</h2>
+          {/* <strong>{weather.weather[0].main}</strong> */}
+          <strong>{JSON.stringify(weather)}</strong>
+          <p>Temprature: {JSON.stringify(weather.main.temp)}° Celsius</p>
+          <Img img={`http://openweathermap.org/img/wn/${weathericon}@2x.png`} />
+          <p>Wind: {JSON.stringify(weather.wind.speed)} m/s</p>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Countries;
